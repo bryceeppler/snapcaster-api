0f1e00b922fbd093511187b980f2fea5f725e83c
Bryce Eppler <eppler97@gmail.com>
2022-08-24 15:50:58 -0700

    adding corsheaders

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     "drf_yasg", #swaggerui
+    'corsheaders',
 
 ]
 
